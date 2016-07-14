4983d51520285e3c9fbc42e03a4da14ffd53dfaf
srinidhik <srinidhi.kambalapally@gmail.com>
2016-07-14 16:11:50 +0530

    main

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ class Main(object):
     e = Event()
 
     while(1):
-        print "1: add_event  2: read_event  3: update_event  4: delete_event  5: list_events_in_date_range "
+        print "1: add_event  2: read_event  3: update_event  4: delete_event  5: upcoming_and_completed events  6: list_event_by_date_or_by_city  7: list_events_in_date_range  8: Exit"
 
         choice = int(raw_input("choice"))
 
@@ -22,16 +22,16 @@ class Main(object):
                                 int(raw_input("enter day(dd) :")))))
             m.add_event(e)
 
-        if choice == 2:
+        elif choice == 2:
             m.read_event_by_id(raw_input("enter the event id :"))
 
-        if choice == 3:
+        elif choice == 3:
             m.update_event_by_id(raw_input("enter the event id :"),raw_input("enter the key to be changed :"), raw_input("enter the changes"))
 
-        if choice == 4:
+        elif choice == 4:
             m.delete_event_by_id(raw_input("enter the event id :"))
 
-        if choice == 5:
+        elif choice == 5:
             m.events_in_date_range(str(date(int(raw_input("enter year(yyyy) :")),
                                             int(raw_input("enter month(mm) :")),
                                             int(raw_input("enter day(dd) :")))),
@@ -39,8 +39,17 @@ class Main(object):
                                             int(raw_input("enter month(mm) :")),
                                             int(raw_input("enter day(dd) :")))))
 
-        elif choice == 10:
+        elif choice == 6:
+            pass
+
+        elif choice == 7:
+            pass
+
+        elif choice == 8:
             break
+
+        else:
+            print "Invalid choice."
 
 #######################################
 
